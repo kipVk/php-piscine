6b45c5706d4d302e4b6fe5f801702a314dd9e0c4
rcenamor <rcenamor@student.hive.fi>
2020-11-13 13:38:59 +0200

    Modfied spaces I noticed on the example and the quotes

--- d04/ex00/index.php
+++ d04/ex00/index.php
@@ -1,17 +1,16 @@
-  
 <?php
 	session_start();
-	if ($_GET['submit']=='OK') {
-		$_SESSION['login']=$_GET['login'];
-		$_SESSION['passwd']=$_GET['passwd'];
+	if ($_GET["submit"] == "OK") {
+		$_SESSION["login"] = $_GET["login"];
+		$_SESSION["passwd"] = $_GET["passwd"];
 	}
 ?>
 
 <html><body>
-<form action='index.php' method='GET'>
-	Username: <input type='text' name='login' value='<?php echo $_SESSION['login']; ?>'/>
+<form action="index.php" method="GET" >
+	Username: <input type="text" name="login" value="<?php echo $_SESSION["login"]; ?>" />
 	<br/>
-	Password: <input type='password' name ='passwd' value='<?php echo $_SESSION['passwd']; ?>'/>
-	<input type='submit' name='submit' value='OK'/>
+	Password: <input type="password" name ="passwd" value="<?php echo $_SESSION["passwd"]; ?>" />
+	<input type="submit" name="submit" value="OK" />
 </form>
 </body></html>
