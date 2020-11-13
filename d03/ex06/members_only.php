f6c263b9d349c6c39119f7402ac14b7bb5a181ed
rcenamor <rcenamor@student.hive.fi>
2020-11-13 23:14:05 +0200

    Fixed error with browser side. Wasn't showing the form

--- d03/ex06/members_only.php
+++ d03/ex06/members_only.php
@@ -1,24 +1,14 @@
 <?php
-    if (isset($_SERVER["PHP_AUTH_USER"]) && isset($_SERVER["PHP_AUTH_PW"]))
+    if ($_SERVER['PHP_AUTH_USER'] == 'zaz' && $_SERVER['PHP_AUTH_PW'] == 'Ilovemylittleponey')
     {
-        echo "<html><body>";
-        $users = array();
-        $users["zaz"] = "Ilovemylittleponey";
-        $name = $_SERVER["PHP_AUTH_USER"];
-        $pass = $_SERVER["PHP_AUTH_PW"];
-        if (isset($users[$name]) && $users[$name] == $pass)
-        {
-            echo "\nHello Zaz<br />\n";
-            echo "<img src='data:image/png;base64,";
-            echo base64_encode(file_get_contents("../img/42.png"));
-            echo "'>\n</body></html>\n";
-        }
-        else
-        {
-            header("WWW-Authenticate:  Basic realm=''Member area''");
-            header("HTTP/1.0 401 Unauthorized");
-            echo "That area is accessible for members only";
-            echo ("</body></html>\n");
-        }
+        echo "<html><body>\nHello Zaz<br />\n<img src='data:image/png;base64,";
+        echo base64_encode(file_get_contents("../img/42.png"));
+        echo "'>\n</body></html>\n";
+    }
+    else
+    {
+        header("WWW-Authenticate:  Basic realm=''Member area''");
+        header("HTTP/1.0 401 Unauthorized");
+        echo "<html><body>That area is accessible for members only</body></html>\n";
     }
 ?>
