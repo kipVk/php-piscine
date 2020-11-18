25ace1635acb7048ccccb8fad872991e5d00b363
rcenamor <rcenamor@student.hive.fi>
2020-11-18 15:29:41 +0200

    Prototyped construct and destruct for Render class

--- d06/ex05/Render.class.php
+++ d06/ex05/Render.class.php
@@ -5,12 +5,34 @@
 		const VERTEX 	= 'VERTEX';
 		const EDGE 		= 'EDGE';
 		const RASTERIZE = 'RASTERIZE';
-		
+
+		private $_width;
+		private $_height;
+		private $_filename;
+
 		public static $verbose = FALSE;
 		
 		public static function doc()
 		{
 			return file_get_contents('Render.doc.txt') . PHP_EOL;
+		}
+
+		public function __construct($info)
+		{
+			if (!isset($info['width']) || !isset($info['height'])
+				|| !isset($info['filename']))
+				return FALSE;
+			$this->_width = $info['width'];
+			$this->_height = $info['height'];
+			$this->_filename = $info['filename'];
+			if (self::$verbose)
+				echo "Render instance constructed" . PHP_EOL;
+		}
+
+		public function __destruct()
+		{
+			if (self::$verbose)
+				echo "Render instance destructed" . PHP_EOL;
 		}
 	}
 ?>
