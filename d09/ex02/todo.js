1b228c3bb3c0ae6331eb03741de4513d32faa812
rcenamor <rcenamor@student.hive.fi>
2020-11-20 18:15:57 +0200

    Cookies working, yeaah

--- d09/ex02/todo.js
+++ d09/ex02/todo.js
@@ -1,27 +1,28 @@
-var cookie_prefix = "td_cookie_";
+var cookie_prefix = "tdCookie_";
 var cookies = document.cookie.split(";");
 
 window.onload = function ()
 {
 	var cookie_value;
+	var todo_id;
 	for (var i = 1; i <= cookies.length; i++)
 	{
 		if (cookies[i - 1].includes(cookie_prefix, 0))
 		{
+			todo_id = cookies[i - 1].split("=")[0].split("_")[1];
 			cookie_value = cookies[i - 1].split("=")[1];
-			add_todo(cookie_value);
+			add_todo(cookie_value, todo_id);
 		}
 	}
 };
 
-function add_todo(todo)
+function add_todo(todo, todo_id)
 {
 	var new_todo_div = document.createElement("div");
 	new_todo_div.classList.add("todo_elem");
 	new_todo_div.innerHTML += todo;
 	var parent_div = document.getElementById("ft_list");
-	var todo_num = parent_div.getElementsByTagName('*').length;
-	new_todo_div.setAttribute("id", todo_num);
+	new_todo_div.setAttribute("id", todo_id);
 	parent_div.insertBefore(new_todo_div, parent_div.firstChild);
 	new_todo_div.setAttribute("onclick", "delete_todo(this)");
 	setCookie(cookie_prefix + new_todo_div.id, todo);
@@ -32,7 +33,7 @@ function add_new_todo()
 	var new_todo = prompt("Write your next TODO");
 	if (new_todo != null && new_todo != "")
 	{
-		add_todo(new_todo);
+		add_todo(new_todo, Date.now());
 	}
 }
 
@@ -53,4 +54,3 @@ function delete_todo(todo)
 		document.cookie = cookie_prefix + todo.id + "=; expires=Thu, 01 Jan 1970 00:00:00 UTC";
 	}
 }
-
